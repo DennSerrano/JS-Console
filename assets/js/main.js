f44bcd7bae3e925869bd65ba7aac93ee78fc7213
Dennserrano <dennserrano@gmail.com>
2021-10-25 14:57:19 +0200

    JSConsole

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+a = 12;
+b = a * 3;
+console.log(b);
+
+document.write(b) 
+console.log(document.URL)
+console.log("Hello world!")
+console.log("Anton")
+
+let name = "Anton "
+console.log(name)
+
+let age = 29 
+console.log(age)
+
+let job = "Trainer "
+console.log(job)
+
+let married = true;
+
+console.log(married)
+console.log(name + "ist " + age + " Jahre alt von Beruf "+ job +"und ist verheiratet:" + married)
